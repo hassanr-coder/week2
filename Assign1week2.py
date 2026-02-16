20387744953eaa49215fe62807732fb37e4d7d03
hassanr-coder <hassanr@students.trinitydc.edu>
2026-02-16 03:00:31 -0500

    new

---
+++ Assign1week2.py
@@ -0,0 +1,14 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 2 Assignment 1
+'''
+name=input("What is your name? ")
+age=int(input("What is your age? "))
+valueofpi=float(input("What is the value of pi? ")) 
+areyouhuman=input("Are you human? (True/false) ")   
+print("Your name is " + name)
+print("Your age is " + str(age))
+print("The value of pi is " + str(valueofpi))
+print("Are you human? " + areyouhuman)
---
+++ perimeter_of_rectangle.py
@@ -0,0 +1,17 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 2 Assignment 2
+'''
+# Ask the user length of the rectangle
+length = float(input("Enter length:"))
+
+# Ask the user width of the rectangle
+width = float(input("Enter width:"))
+
+# Calculate the perimeter of the rectangle
+parimeter = 2 * (length + width)
+
+# Display the print results
+print("The perimeter of the rectangle is:", parimeter)
