12f81d3238c26735f6530d6284995a2b30ed8825
hassanr-coder <hassanr@students.trinitydc.edu>
2026-02-17 14:04:23 -0500

    new

---
+++ number_check.py
@@ -0,0 +1,12 @@
+'''
+Rayaan Hassan
+CMSC 111
+Spring 2026
+Week 2 Assignment 3
+'''
+if number > 0:
+    print("The number is positive.")
+elif number < 0:
+    print("The number is negative.")
+else:
+    print("The number is zero.")    
