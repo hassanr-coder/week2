32c88f39e0b86619d71be6d3a914dc37bf84ef82
hassanr-coder <hassanr@students.trinitydc.edu>
2026-02-27 02:48:38 -0500

    Create

--- login_system.py
+++ login_system.py
@@ -4,3 +4,16 @@ CMSC 111
 Spring 2026
 Week 2 Assignment 4
 '''
+# Correct username and password
+correct_username = "admin"
+correct_password = "password123"
+
+# Ask the user for their username and password
+username = input("Enter your username: ")
+password = input("Enter your password: ")
+
+# Verfi the username and password is correct
+if username == correct_username and password == correct_password:
+    print("Login successful")
+else:
+    print("Invalid credentials")  
