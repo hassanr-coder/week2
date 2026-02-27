c2b23cd229388f9c90e3475cf73799d5c929d0cb
hassanr-coder <hassanr@students.trinitydc.edu>
2026-02-27 02:43:16 -0500

    create

---
+++ login_system.py
@@ -0,0 +1,6 @@
+'''
+Rayaan Hassan 
+CMSC 111
+Spring 2026
+Week 2 Assignment 4
+'''
--- number_check.py
+++ number_check.py
@@ -4,6 +4,8 @@ CMSC 111
 Spring 2026
 Week 2 Assignment 3
 '''
+number = float(input("Enter a number: "))
+
 if number > 0:
     print("The number is positive.")
 elif number < 0:
